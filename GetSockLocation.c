a85c87cb9d15b8f343ea7e4b5e09d1056a41747b
Aswini S <ashumeow@live.com>
2014-05-12 02:24:35 +0530

    Create GetSockLocation.c

---
+++ GetSockLocation.c
@@ -0,0 +1,10 @@
+#include "meow.h"
+int sockfd_to_family(int sockfd)
+{
+	struct sockaddr_storage ss;
+	socklen_t len;
+	len=sizeof(ss);
+	if(getsockname(sockfd,(SA *) &ss, &len) < 0)
+		return(-1);
+	return(ss.ss_family);
+}
