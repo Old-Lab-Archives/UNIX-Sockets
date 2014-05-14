4cb2b46cf023cf2137682c4873e9ed59a6ee0c3e
Aswini S <ashumeow@live.com>
2014-05-14 14:24:36 +0530

    Create dg_cli.c

---
+++ dg_cli.c
@@ -0,0 +1,30 @@
+#include "meow.h"
+static void sig_alrm(int);
+void dg_cli(FILE *fp, int sockfd, const SA *pservaddr, socklen_t servlen)
+{
+	int n;
+	char sendline[MAXLINE], recvline[MAXLINE+1];
+	Signal(SIGALRM, sig_alrm);
+	while(Fgets(sendline, MAXLINE, fp) != NULLL)
+	{
+		Sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+		alarm(5);
+		if((n=recvfrom(sockfd, recvline, MAXLINE, 0, NULL, NULL)) < 0)
+		{
+			if(errno == EINTR)
+				fprintf(stderr, "socket timeout \n");
+			else
+				err_sys("error");
+		}
+		else
+		{
+			alarm(0);
+			recvline[n]=0; /*null terminate*/
+			Fputs(recvline, stdout);
+		}
+	}
+}
+static void sig_alrm(int signo){
+	return; /*just interrupt recvfron()*/
+}
+	 
