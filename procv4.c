4821ffba15f798488afc5a6c8db87decc23d945d
Aswini S <ashumeow@live.com>
2014-05-18 21:35:45 +0530

    Update procv4.c

--- procv4.c
+++ procv4.c
@@ -1,5 +1,4 @@
 /* This program uses proc_v4 function which processes ICMPv4 message */
-/*AshuMeow*/
 /* proc_v4 syntax
 void proc_v4(char *, ssize_t, struct msghdr *, struct timeval *);
 (refer <ping.h> ) */
@@ -18,6 +17,7 @@ void proc_v4(char *ptr, ssize_t len, struct msghdr *msg, struct timeval *tvrecv)
 	icmp=(struct icmp *) (ptr + hlen1); /* ICMP header */
 	if((icmplen=len-hlen1) < 8)
 		return; /* bad packets */
+	/* Checking for ICMP echo reply */
 	if(icmp->icmp_type==ICMP_ECHOREPLY)
 	{
 		if(icmp->icmp_id!=pid)
@@ -27,11 +27,14 @@ void proc_v4(char *ptr, ssize_t len, struct msghdr *msg, struct timeval *tvrecv)
 		tvsend=(struct timeval *)
 			icmp->icmp_data;
 		tv_sub(tvrecv, tvsend);
+		/* calculating rtt by subtracting the time the message was sent from current time.
+		Then, rtt is converted from microseconds to milliseconds */
 		rtt=tvrecv->tv_sec * 1000.0 + tvrecv->tv_usec / 1000.0;
 		printf("%d bytes from %s: seq=%u, ttl=%d, rtt=%.3f \n",
 			icmplen, Sock_ntop_host(pr->sarecv, pr->salen),
 			icmp->icmp_seq, ip->ip_ttl, rtt);
 	}
+	/* printing all recived ICMP message only if verbose option is specified */
 	else if(verbose)
 	{
 		printf("%d bytes from %s: type=%d, code=%d \n",
@@ -39,3 +42,4 @@ void proc_v4(char *ptr, ssize_t len, struct msghdr *msg, struct timeval *tvrecv)
 			icmp->icmp_type, icmp->icmp_code);
 	}
 }
+/*AshuMeow*/
