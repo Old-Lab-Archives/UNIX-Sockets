6ef44265e30ca060436f572cfe21e2abc534d836
Aswini S <ashumeow@live.com>
2014-05-18 21:00:19 +0530

    Update ping_readloop.c

--- ping_readloop.c
+++ ping_readloop.c
@@ -8,19 +8,24 @@ void readloop(void)
 	struct iovec iov;
 	ssize_t n;
 	struct timeval tval;
+	/* creating socket */
 	sockfd=Socket(pr->sasend->sa_family,SOCK_RAW,pr->icmpproto);
 	setuid(getuid()); /*special permissions not required*/
+	/* Performing protocol specific initialization */
 	if(pr->finit)
 		(*pr->finit) ();
+	/* Setting socket receive buffer size */
 	size=60*1024; /* okay if setsockopt fails */
 	setsockopt(sockfd,SOL_SOCKET,SO_RCVBUF,&size,sizeof(size));
 	sig_alrm(SIGALRM); /*sending first packet*/
+	/* Setting up msghdr for recvmsg */
 	iov.iov_base=recvbuf;
 	iov.iov_len=sizeof(recvbuf);
 	msg.msg_name=pr->sarecv;
 	mas.msg_iov= &iov;
 	msg.msg_iovlen=1;
 	msg.msg_control=controlbuf;
+	/* Infinite loop reading all ICMP messages */
 	for(; ;)
 	{
 		msg.msg_namelen=pr->salen;
