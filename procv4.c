83011e24becaa71358106cd54d6134037418a859
Aswini S <ashumeow@live.com>
2014-05-18 21:31:01 +0530

    Create procv4.c

---
+++ procv4.c
@@ -0,0 +1,41 @@
+/* This program uses proc_v4 function which processes ICMPv4 message */
+/*AshuMeow*/
+/* proc_v4 syntax
+void proc_v4(char *, ssize_t, struct msghdr *, struct timeval *);
+(refer <ping.h> ) */
+#include "ping.h"
+void proc_v4(char *ptr, ssize_t len, struct msghdr *msg, struct timeval *tvrecv)
+{
+	int hlen1, icmplen;
+	double rtt;
+	struct ip *ip;
+	struct icmp *icmp;
+	struct timeval *tvsend;
+	ip=(struct ip *) ptr; /* start of IP header */
+	hlen1=ip->ip_h1 << 2; /* length of IP header */
+	if(ip->ip_p!=IPPROTO_ICMP)
+		return; /* not ICMP */
+	icmp=(struct icmp *) (ptr + hlen1); /* ICMP header */
+	if((icmplen=len-hlen1) < 8)
+		return; /* bad packets */
+	if(icmp->icmp_type==ICMP_ECHOREPLY)
+	{
+		if(icmp->icmp_id!=pid)
+			return; /* Not a response to ECHO_REQUEST */
+		if(icmplen<16)
+			return; /* not enough data to use */
+		tvsend=(struct timeval *)
+			icmp->icmp_data;
+		tv_sub(tvrecv, tvsend);
+		rtt=tvrecv->tv_sec * 1000.0 + tvrecv->tv_usec / 1000.0;
+		printf("%d bytes from %s: seq=%u, ttl=%d, rtt=%.3f \n",
+			icmplen, Sock_ntop_host(pr->sarecv, pr->salen),
+			icmp->icmp_seq, ip->ip_ttl, rtt);
+	}
+	else if(verbose)
+	{
+		printf("%d bytes from %s: type=%d, code=%d \n",
+			icmplen, Sock_ntop_host(pr->sarecv, pr->salen),
+			icmp->icmp_type, icmp->icmp_code);
+	}
+}
